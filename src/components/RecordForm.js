710ead08913d6e660c6bc2e2cd10d1b60df78575
tervingo <keltivas@gmail.com>
2025-08-15 11:10:07 +0200

    aðvarun > aðvörun

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -343,7 +343,7 @@ const RecordForm = ({
                     onChange={(e) => updateLeak(index, 'aðvarun', e.target.checked)}
                     className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                   />
-                  <span className="text-sm font-medium text-gray-700">Aðvarun</span>
+                  <span className="text-sm font-medium text-gray-700">Aðvörun</span>
                 </label>
               </div>
             </div>
